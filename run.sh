631d4fd70dd41468160b80f488d65290c9e0d089
totalo <totalo@apache.org>
2025-06-06 00:11:21 +0800

    build: Temporarily cancel the conversion to Word version (#43)

--- run.sh
+++ run.sh
@@ -9,10 +9,10 @@ latexmk -xelatex -shell-escape main.tex
 # compile the ready-for-word version
 # - rasterized images
 # - [TODO] fix text size after \cite{}
-rm -rf for-word/
-mkdir for-word/
-cp -r chapters figures hnunumerical.bst hnuthesis.cls main.tex references.bib for-word/
-python for-word.py --source=./for-word --download_path=./download
-cd for-word/
-latexmk -xelatex -shell-escape main.tex
-mv main.pdf ../main-for-word.pdf
+# rm -rf for-word/
+# mkdir for-word/
+# cp -r chapters figures hnunumerical.bst hnuthesis.cls main.tex references.bib for-word/
+# python for-word.py --source=./for-word --download_path=./download
+# cd for-word/
+# latexmk -xelatex -shell-escape main.tex
+# mv main.pdf ../main-for-word.pdf
